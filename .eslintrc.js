31dfbf9f5707d317ed72d0832745111575554c73
Trevor Landau <landau@users.noreply.github.com>
2019-04-07 06:35:59 -0400

    update eslintrc for ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['@landau/eslint-config', '@landau/eslint-config/rules/jsdoc'],
+  extends: ['@landau/eslint-config', '@landau/eslint-config/rules/ts'],
   plugins: ['mocha'],
   rules: {
     'prefer-destructuring': 'off',
@@ -8,66 +8,35 @@ module.exports = {
     /**
      * @see https://github.com/lo1tuma/eslint-plugin-mocha/blob/master/docs/rules/README.md
      */
-
-    /**
-     * enforces handling of callbacks for async tests
-     */
+    // enforces handling of callbacks for async tests
     'mocha/handle-done-callback': 'error',
-    /**
-     * disallow exclusive mocha tests
-     */
+    // disallow exclusive mocha tests
     'mocha/no-exclusive-tests': 'error',
-    /**
-     * disallow global tests
-     */
+    // disallow global tests
     'mocha/no-global-tests': 'warn',
-    /**
-     * disallow hooks for a single test or test suite
-     */
+    // disallow hooks for a single test or test suite
     'mocha/no-hooks-for-single-case': 'off',
-    /**
-     * disallow hooks
-     */
+    // disallow hooks
     'mocha/no-hooks': 'off',
-    /**
-     * disallow identical titles
-     */
+    // disallow identical titles
     'mocha/no-identical-title': 'warn',
-    /**
-     * disallow arrow functions as arguments to mocha globals
-     */
+    // disallow arrow functions as arguments to mocha globals
     'mocha/no-mocha-arrows': 'off',
-    /**
-     * disallow pending/unimplemented mocha tests
-     */
+    // disallow pending/unimplemented mocha tests
     'mocha/no-pending-tests': 'off',
-    /**
-     * disallow returning in a test or hook function that uses a callback
-     */
+    // disallow returning in a test or hook function that uses a callback
     'mocha/no-return-and-callback': 'error',
-    /**
-     * disallow duplicate uses of a hook at the same level inside a describe
-     */
+    // disallow duplicate uses of a hook at the same level inside a describe
     'mocha/no-sibling-hooks': 'warn',
-    /**
-     * disallow skipped mocha tests (fixable)
-     */
+    // disallow skipped mocha tests (fixable)
     'mocha/no-skipped-tests': 'warn',
-    /**
-     * disallow synchronous tests
-     */
+    // disallow synchronous tests
     'mocha/no-synchronous-tests': 'off',
-    /**
-     * disallow top-level hooks
-     */
+    // disallow top-level hooks
     'mocha/no-top-level-hooks': 'error',
-    /**
-     * match suite descriptions against a pre-configured regular expression
-     */
+    // match suite descriptions against a pre-configured regular expression
     'mocha/valid-suite-description': 'warn',
-    /**
-     * match test descriptions against a pre-configured regular expression
-     */
+    // match test descriptions against a pre-configured regular expression
     'mocha/valid-test-description': 'off'
   }
 };
